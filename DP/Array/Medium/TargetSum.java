0ab44154a95d2527cd1f78ca9d4a67c9c246d6d7
Rohit Kumar <rohitkumar65108@gmail.com>
2025-01-01 12:32:17 +0530

    494

--- DP/Array/Medium/TargetSum.java
+++ DP/Array/Medium/TargetSum.java
@@ -1,75 +1,48 @@
-//Leetcode : 689. Maximum Sum of 3 Non-Overlapping Subarrays
+//Leetcode: 494. Target Sum
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    private int[][] dp = new int[20001][4];
-    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-        //Approach : Recursion memoisation
-
-        // Initialize the memoization array with -1
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+    HashMap<String,Integer> map = new HashMap<>();
+    int t[][];
+    int totalSum = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+        //
+        int n = nums.length;
+        for(int num: nums){
+            totalSum+=num;
         }
-        int n = nums.length -k +1;
-        int subarraySum[] = new int[n];
-        int i=0;
-        int j=0;
-        int windowSum = 0;
+        t = new int[n][2*totalSum+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(t[i],-1);
+        }
+        return func(nums,target,n,0,0);
+    }
 
-        //sliding window algorithm
-        while(j<nums.length){
-            windowSum+=nums[j];
-
-            if(j-i+1==k){
-                subarraySum[i] = windowSum;
-                windowSum-=nums[i];
-                i++;
+    public int func(int nums[],int target,int n , int index,int sum){
+        if(index>=n){
+            if(sum==target){
+                return 1;
             }
-            j++;
-        }
-
-        List<Integer> indices = new ArrayList<>();
-        
-        solve(subarraySum,k,0,3,indices);
-
-        return indices.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    public void solve(int sums[],int k,int index,int count,List<Integer> indices){
-        if(count==0 || index>=sums.length){
-            return;
-        }
-
-        int take = sums[index] + helper(sums,k,index+k,count-1);
-        int not_take = helper(sums,k,index+1,count);
-
-        if(take>=not_take){
-            indices.add(index);
-            solve(sums,k,index+k,count-1,indices);
-        }
-        else{
-            solve(sums,k,index+1,count,indices);
-        }
-    }
-
-    public int helper(int sums[],int k,int index,int count){
-        if(count==0){
             return 0;
         }
-
-        if(dp[index][count]!=-1){
-            return dp[index][count];
+        //String key = ""+index+"_"+sum;
+        // if(map.containsKey(key)==true){
+        //     return map.get(key);
+        // }
+        if(t[index][sum+totalSum]!=-1){
+            return t[index][sum+totalSum];
         }
-        if(index>=sums.length){
-            return Integer.MIN_VALUE;
-        }
 
-        int take = sums[index] + helper(sums,k,index+k,count-1);
-        int not_take = helper(sums,k,index+1,count);
+        //take +ve 
+        int pos = func(nums,target,n,index+1,sum+nums[index]);
+        //take -ve
+        int neg = func(nums,target,n,index+1,sum-nums[index]);
 
-        return dp[index][count] = Math.max(take,not_take);
+        //map.put(key,pos+neg);
+
+        return t[index][sum+totalSum]= pos+neg;
     }
+
+    
 }
