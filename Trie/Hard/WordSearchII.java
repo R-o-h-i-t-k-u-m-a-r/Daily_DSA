4da0d93221d67ed54df98d6872c7849668482e8b
Rohit Kumar <rohitkumar65108@gmail.com>
2025-01-13 10:17:59 +0530

    212

--- Trie/Hard/WordSearchII.java
+++ Trie/Hard/WordSearchII.java
@@ -1,22 +1,24 @@
 package Trie.Hard;
 
+import java.util.*;
+
 //Leetcode : 212. Word Search II
 
 class TrieNode {
-    boolean endOfWord;
+    TrieNode child[];
     String word;
-    TrieNode children[];
+    boolean endOfWord;
 
     TrieNode(){
-        this.endOfWord = false;
-        this.word = "";
-        this.children = new TrieNode[26];
+        child = new TrieNode[26]; //all small case alphabets 
+        word = "";
+        endOfWord = false;
     }
 }
 
 class Trie {
     TrieNode root;
-
+    //Initialize the root
     Trie(){
         root = new TrieNode();
     }
@@ -24,66 +26,81 @@ class Trie {
     public void insert(String word){
         TrieNode current = root;
         for(char ch: word.toCharArray()){
-            int index = ch-'a';
-            if(current.children[index]==null){
-                current.children[index] = new TrieNode();
+            int index = ch - 'a';
+            if(current.child[index]==null){
+                current.child[index] = new TrieNode(); //if child is not present then create new child node
             }
-            current = current.children[index];
+            current = current.child[index]; //move pointer to it's child node
         }
-        current.endOfWord = true;
-        current.word = word;
+        current.word = word; //assign string word after inserting all the characters
+        current.endOfWord = true; //mark true end of word
     }
 }
+
+
 class Solution {
     int row;
     int col;
-    int directions[][] = {{-1,0},{1,0},{0,1},{0,-1}};
+    int directions[][] = {{1,0},{0,1},{-1,0},{0,-1}}; //top right bottom left
 
-    public void DFS(char[][] board,int i,int j, TrieNode root, List<String> result){
+    public void DFS(int i,int j,char[][] board,TrieNode root,ArrayList<String> result){
         //base case
-        if(i<0 || i>=row || j<0 || j>=col || board[i][j]=='$' || root.children[board[i][j]-'a']==null){
+        if(i<0 || i>=row || j<0 || j>=col || board[i][j]=='$' || root.child[board[i][j]-'a']==null){
             return;
         }
-
-        root = root.children[board[i][j]-'a'];
+        //shift root node to it's child node
+        int index = board[i][j] - 'a';
+        root = root.child[index];
+        //if we have find the word in trienode then store into result
         if(root.endOfWord==true){
             result.add(root.word);
+            //mark false of end of word for duplicate safety
             root.endOfWord = false;
         }
-
+        
         char temp = board[i][j];
+        //mark visited in board char cell for revisit safety
         board[i][j] = '$';
 
+        //explore all four direction for current board cell character to matched any word in trie
         for(int dir[]: directions){
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-
-            DFS(board,new_i,new_j,root,result);
+            int new_row = i + dir[0];
+            int new_col = j + dir[1];
+            //search this new board cell character into trienode 
+            DFS(new_row,new_col,board,root,result);
         }
+
+        //after exploring all four directions re-assign board cell character to previous 
         board[i][j] = temp;
     }
     public List<String> findWords(char[][] board, String[] words) {
         //Approach : Using Trie
         row = board.length;
         col = board[0].length;
+
+        //create root node trie
         Trie trie = new Trie();
-        List<String> result = new ArrayList<>();
-        //insert all the words into Trie
+
+        //insert all the words into trie 
         for(String word: words){
             trie.insert(word);
         }
 
-        //Traverser to board to find all the word occurance
+        //creating list to store word
+        ArrayList<String> result = new ArrayList<>();
+
+        //Now iterate to board matrix to find word present in trie
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
+                //start searching word which first character present in trie
                 char ch = board[i][j];
-                if(trie.root.children[ch-'a']!=null){
-                    DFS(board,i,j,trie.root,result);
+                if(trie.root.child[ch-'a']!=null){
+                    DFS(i,j,board,trie.root,result);
                 }
             }
         }
 
         return result;
-        
     }
 }
+
